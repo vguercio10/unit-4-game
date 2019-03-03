da0384827fe78829ae4daa0339864b0ffb943799
Jessica Jones <47366649+vguercio10@users.noreply.github.com>
2019-03-03 16:04:48 -0500

    reset added

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var targetPoints = (x);
+
+function targetPoints (max,min) {
+    $("#x").text(Math.floor(Math.random() * 120) + 19);
+  }
+  targetPoints(120, 9);
+  console.log(targetPoints);
+
+
+crystalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+ for (i = crystalNumbers; i , crystalNumbers.length; i++);
+  
